2a17082330268e193858d1e53db0f18f1b9c5816
Melange93 <lakatos.reka.veronika@gmail.com>
2019-09-17 10:37:30 +0200

    basic js

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,13 @@
+const colors = 'dchs';
+const values = '234567890jqka';
+let full_deck = [];
+let amount_of_decks = 3;
+
+
 function main() {
+    for (let color of colors) {
+        console.log(color)
+    }
 
 }
 
