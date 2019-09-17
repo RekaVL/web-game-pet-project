231a0503d69dc683e3a73916963111b0b4cf1f16
BotondSiklosi <kissiki99@gmai.com>
2019-09-17 14:28:05 +0200

    game start with 2 cards

--- static/js/init.js
+++ static/js/init.js
@@ -20,22 +20,18 @@ export let init = {
             hitButton.textContent = 'Hit';
             container.appendChild(hitButton);
 
-            init.initTest();
+
 
 
             let isOver = false;
             let newDeck = [];
             const fullDeck = deal.createDeck(1);
-
-            let userHand = deal.dealCards(2, newDeck, fullDeck);
-
-            console.log(userHand);
-
             const btnHit = document.querySelector('.hit-btn');
             btnHit.addEventListener('click', function () {
                 userHand.push(deal.dealCards(1, newDeck, fullDeck)[0]);
                 console.log(userHand);
             });
+            init.initTest(newDeck, fullDeck);
         });
     },
 
@@ -43,18 +39,17 @@ export let init = {
         let newCard = document.createElement('img');
         newCard.classList.add(`test-card${cardContainer.childElementCount + 1}`);
         newCard.setAttribute('src', `../static/images/${card}.png`);
-        cardContainer.appendChild(newCard)
+        cardContainer.appendChild(newCard);
+        newCard.dataset.value = `${card.slice(0, 1)}`;
+
     },
 
-    initTest: function () {
+    initTest: function (newDeck, fullDeck) {
         let cardContainer = document.querySelector('.cards');
-        console.log(cardContainer);
-        document.addEventListener('keydown', (e) => {
-            if (e.key === 'Escape') {
-                init.addTestCard('AS', cardContainer);
-                init.addTestCard('10S', cardContainer);
-                init.addTestCard('QH', cardContainer);
-            }
-        });
+        for (let card of deal.dealCards(2, newDeck, fullDeck)) {
+            init.addTestCard(`${card.slice(0, 2)}`, cardContainer);
+            console.log(`${card.slice(0, 1)}`)
+        }
+
     }
 };
