964c72dea69aaf7abf2f240a8fb0d4046fb79180
Melange93 <lakatos.reka.veronika@gmail.com>
2019-09-18 14:35:39 +0200

    Merge conflict problem resolved

--- static/js/init.js
+++ static/js/init.js
@@ -2,8 +2,6 @@ import {deal} from "./dealLogic.js";
 
 export let init = {
     init: function () {
-
-        console.log(document.querySelector('input[name="deck"]:checked').value)
         let startButton = document.querySelector('#start-btn');
         startButton.addEventListener('click', function () {
             let deckStyle = document.querySelector('input[name="deck"]:checked').value;
@@ -30,20 +28,16 @@ export let init = {
             cards.classList.add('cards');
             container.appendChild(cards);
 
-
             let isOver = false;
             let newDeck = [];
             const fullDeck = deal.createDeck(1);
             const hands = {dealer: [], player: []};
 
-
             const btnHit = document.querySelector('.hit-btn');
             btnHit.addEventListener('click', function () {
                 let hit = (deal.dealCards(1, newDeck, fullDeck)[0]);
                 hands.player.push(hit[0]);
-                console.log(hands.player);
-                console.log(hit)
-                init.addTestCard(hit, cards)
+                init.addTestCard(hit, cards);
                 init.showCardSum(init.cardCounter())
             });
 
@@ -65,13 +59,13 @@ export let init = {
         let newCard = document.createElement('img');
         newCard.classList.add("playerCard");
         newCard.classList.add('test-card');
-        if (cardContainer.childElementCount + 1 > 1){
+        if (cardContainer.childElementCount + 1 > 1) {
             newCard.style.position = "relative";
             let leftPosition = 40 * cardContainer.childElementCount;
             newCard.style.left = `${leftPosition}px`
         }
         newCard.classList.add(`test-card${cardContainer.childElementCount + 1}`);
-        console.log("card number in line: "+`${cardContainer.childElementCount + 1}`);
+        console.log("card number in line: " + `${cardContainer.childElementCount + 1}`);
         newCard.setAttribute('src', `../static/images/${loadFile[0]}/${card.slice(0, 2)}.${loadFile[1]}`);
         newCard.dataset.value = `${card.slice(0, 1)}`;
         cardContainer.appendChild(newCard);
@@ -90,14 +84,14 @@ export let init = {
         newCard.classList.add("dealerCard");
 
         newCard.classList.add('test-card');
-        if (dealerCardContainer.childElementCount + 1 > 1){
+        if (dealerCardContainer.childElementCount + 1 > 1) {
             newCard.style.position = "relative";
             let leftPosition = 40 * dealerCardContainer.childElementCount;
             newCard.style.left = `${leftPosition}px`
         }
 
         newCard.classList.add(`dealer-card${dealerCardContainer.childElementCount + 1}`);
-        console.log("card number in line: "+`${dealerCardContainer.childElementCount + 1}`);
+        console.log("card number in line: " + `${dealerCardContainer.childElementCount + 1}`);
 
         if (dealerCardContainer.childElementCount + 1 <= 1) {
             newCard.setAttribute('src', `../static/images/${loadFile[0]}/${card.slice(0, 2)}.${loadFile[1]}`)
@@ -105,12 +99,11 @@ export let init = {
             newCard.setAttribute('src', `../static/images/${loadFile[0]}/back.${loadFile[1]}`);
         }
 
-        if (`${dealerCardContainer.childElementCount + 1}`>3 && `${card.slice(0, 1)}`==="A" && init.cardCounter()>10){
+        if (`${dealerCardContainer.childElementCount + 1}` > 3 && `${card.slice(0, 1)}` === "A" && init.cardCounter() > 10) {
             newCard.dataset.value = 1;
-        } else{
+        } else {
             newCard.dataset.value = `${card.slice(0, 1)}`;
         }
-
         dealerCardContainer.appendChild(newCard);
     },
 
@@ -119,24 +112,21 @@ export let init = {
         hands.player = deal.dealCards(2, newDeck, fullDeck);
         for (let card of hands.player) {
             init.addTestCard(`${card}`, cardContainer);
-            console.log(`${card.slice(0, 1)}`)
         }
-
 
         let dealerCardContainer = document.querySelector('.dealer-cards');
         hands.dealer = deal.dealCards(2, newDeck, fullDeck);
         for (let card of hands.dealer) {
             init.dealerAddTestCard(`${card}`, dealerCardContainer);
         }
-
     },
 
     cardCounter: function () {
         let cardSum = 0;
-        let asCounter=0;
+        let asCounter = 0;
         let cards = document.querySelectorAll('.playerCard');
         for (let card of cards) {
-            let value = card.dataset.value
+            let value = card.dataset.value;
             if (value === "0") {
                 value = 10
             } else if (value === "K") {
@@ -146,29 +136,28 @@ export let init = {
             } else if (value === "J") {
                 value = 10
             } else if (value === "A") {
-                asCounter++
-                value=0
+                asCounter++;
+                value = 0
             }
             cardSum = cardSum + parseInt(value)
-        };
+        }
+
         for (let card of cards) {
             let value = card.dataset.value;
             if (value === "A" && cardSum < 11) {
-                if (asCounter>1 && cardSum===10){
-                    value=1;
-                }else{
+                if (asCounter > 1 && cardSum === 10) {
+                    value = 1;
+                } else {
                     value = 11;
                 }
-                cardSum=value+cardSum;
-            } else if (value==="A" && cardSum >10) {
-                value=1;
-                cardSum=value+cardSum;
+                cardSum = value + cardSum;
+            } else if (value === "A" && cardSum > 10) {
+                value = 1;
+                cardSum = value + cardSum;
             } else {
                 ;
             }
-
         }
-        console.log(cardSum)
         return cardSum
     },
 
@@ -179,12 +168,9 @@ export let init = {
         } catch (e) {
             let container = document.querySelector(".container");
             let counterDiv = document.createElement("div")
-            counterDiv.setAttribute("id","cardCount")
-            counterDiv.innerText = value
+            counterDiv.setAttribute("id", "cardCount")
+            counterDiv.innerText = value;
             container.appendChild(counterDiv)
         }
-
     }
-
-
 };
